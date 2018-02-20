c35321feb94863b42666bf9b60c79e660e09eaed
udayakp <kpu1990@gmail.com>
2018-02-21 00:38:40 +0530

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -4,24 +4,25 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-	aimodule.train()
-	return render_template('chat.html')
-    	
+    aimodule.train()
+    return render_template('chat.html')
+    
 @app.route("/ask", methods=['POST'])
 def ask():
-	message = str(request.form['messageText'])
-    	# kernel now ready for use
-    	while True:
-        	if message == "quit":
-			aimodule.record()
-		elif message == "save":
-			aimodule.saveBrain("bot_brain.brn")
-		elif message == "#elp":
-			aimodule.incident()
-		else:
-			bot_response = aimodule.respond(message)
-	    		# print bot_response
-	    		return jsonify({'status':'OK','answer':bot_response})
+    message = str(request.form['messageText'])
+    # kernel now ready for use
+    while True:
+        if message == "quit":
+            aimodule.record()
+	elif message == "save":
+	    aimodule.saveBrain("bot_brain.brn")
+	elif message == "#elp":
+            aimodule.incident()
+	else:
+	    bot_response = aimodule.respond(message)
+	    # print bot_response
+	    return jsonify({'status':'OK','answer':bot_response})
+
 
 if __name__ == "__main__":
     application.run()
