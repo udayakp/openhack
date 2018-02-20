7bb513bfe470bf7664423b1374744ed52062ebf8
udayakp <kpu1990@gmail.com>
2018-02-21 00:16:01 +0530

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -11,16 +11,18 @@ def hello():
 def ask():
     message = str(request.form['messageText'])
     # kernel now ready for use
-	while True:
-	    if message == "quit":
-                aimodule.record()
-	        exit()
-	    elif message == "save":
-	        aimodule.saveBrain("bot_brain.brn")
-	    else:
-	        bot_response = aimodule.respond(message)
-	        # print bot_response
-	        return jsonify({'status':'OK','answer':bot_response})
+    while True:
+        if message == "quit":
+            aimodule.record()
+	    exit()
+	elif message == "save":
+	    aimodule.saveBrain("bot_brain.brn")
+	elif message == "#elp"
+            aimodule.incident()
+	else:
+	    bot_response = aimodule.respond(message)
+	    # print bot_response
+	    return jsonify({'status':'OK','answer':bot_response})
 
 
 if __name__ == "__main__":
